3af298820cde79a0014126541248365fc04c4a48
felipesuri <felipe@felipesuri.com>
2020-12-22 16:10:24 -0300

    Finalizando desafio de componentes

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <navigation></navigation>
+    <app-bar></app-bar>
+    <app-content></app-content>
+    <app-footer></app-footer>
+  </v-app>
+</template>
+
+<script>
+import Navigation from './Navigation.vue';
+import AppBar from './AppBar.vue';
+import Content from './Content.vue';
+import Footer from './Footer.vue';
+
+export default {
+  components: {
+    navigation: Navigation,
+    'app-bar': AppBar,
+    'app-content': Content,
+    'app-footer': Footer,
+  },
+}
+</script>
